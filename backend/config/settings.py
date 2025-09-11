8bc140022aecb6c4becd5f71a9dcd644ab99bb81
Dhanush Shetty <dhanushshettycache@outlook.com>
2025-09-11 20:21:28 +0530

    Fix app names in settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "users",
-    "issues"
+    "users.apps.UsersConfig",
+    "issues.apps.IssuesConfig",
 ]
 
 MIDDLEWARE = [
