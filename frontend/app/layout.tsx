2c7b9beb99a5f4be2a3ebe915dc43901652c3ba6
Dhanush Shetty <dhanushshettycache@outlook.com>
2025-09-13 05:27:38 +0530

    Customize radix-ui theme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from "next";
 import "./globals.css";
 import NavBar from "@/app/NavBar";
 import {Theme} from "@radix-ui/themes";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Issue Tracker",
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen bg-gray-50">
-        <Theme>
+        <Theme accentColor="blue" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
