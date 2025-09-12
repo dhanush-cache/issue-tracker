fdbf94e582e32e4ad29981b7fb6cb1f30900518c
Dhanush Shetty <dhanushshettycache@outlook.com>
2025-09-13 05:20:12 +0530

    Build the new issue page

--- frontend/app/issues/page.tsx
+++ frontend/app/issues/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 export default function IssuesPage() {
-  return <Button>New Issue</Button>;
+  return (
+    <Link href="/issues/new">
+      <Button>New Issue</Button>
+    </Link>
+  );
 }
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <body className="min-h-screen bg-gray-50">
         <Theme>
           <NavBar />
-          <main>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
