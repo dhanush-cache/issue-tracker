cb8b551580bd158e40cd04d9206fda931fa0bcb6
Dhanush Shetty <dhanushshettycache@outlook.com>
2025-09-20 02:56:16 +0530

    Add loading skeletons

--- frontend/app/issues/[id]/loading.tsx
+++ frontend/app/issues/[id]/loading.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Card, Flex, Skeleton } from "@radix-ui/themes";
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading issue...</div>;
+  return (
+    <div>
+      <Skeleton width="100rem" height="2rem" />
+      <Flex gap="3" my="2">
+        <Skeleton width="5rem" height="2rem" />
+        <Skeleton width="8rem" height="2rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+          doloribus enim error, hic in labore laboriosam natus necessitatibus
+          odio perferendis quam rerum unde? Impedit laudantium maiores officia
+          voluptate? Atque, vitae!
+        </Skeleton>
+      </Card>
+    </div>
+  );
 };
 
 export default LoadingIssueDetailPage;
--- frontend/app/issues/new/loading.tsx
+++ frontend/app/issues/new/loading.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import {Skeleton} from "@radix-ui/themes";
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <Skeleton width="35rem" height="2rem" className="mb-5" />
+      <Skeleton width="35rem" height="20rem" className="mb-5" />
+    </div>
+  );
 };
 
 export default LoadingNewIssuePage;
