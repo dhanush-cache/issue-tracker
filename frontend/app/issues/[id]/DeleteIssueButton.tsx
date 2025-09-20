1e0b42a7d56f5a0077b7b56fadecb7f748775f0d
Dhanush Shetty <dhanushshettycache@outlook.com>
2025-09-21 03:41:45 +0530

    Show a delete conformation dialog

--- frontend/app/issues/[id]/DeleteIssueButton.tsx
+++ frontend/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,35 @@
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { Issue } from "@/app/issues/page";
 import { TrashIcon } from "@radix-ui/react-icons";
 
 export const DeleteIssueButton = (props: { issue: Issue }) => {
   return (
-    <Button color="red">
-      <TrashIcon />
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Revoke access</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
